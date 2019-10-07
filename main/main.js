7673d41bf94a55e33f3bf5c9e7c18ab4c4aa5b8d
LESTER JAN ARTIENDA (PLAN-IRIS-ISD-OOCLL/MNL) <ARTIELE@OOCL.COM>
2019-10-07 17:32:27 +0800

    Fixed bug on subtotal and number of decimal

--- main/main.js
+++ main/main.js
@@ -3,20 +3,16 @@ module.exports = function main(input) {
     return renderText(out);
 };
 
-function isInObject(object,val){
-    return object.some(ov => ov.Name === val)
-}
-
 function aggregateSubtotal(input) {
     let arr = [];
     input.forEach(e => {
         if (arr.length === 0 || isInObject(arr,e.Name) === false) {
-            arr.push({'Name':e.Name, 'Quantity': 1, 'Unit price': parseFloat(e.Price).toFixed(2), 'Subtotal': e.Price})
+            arr.push({'Name':e.Name, 'Quantity': 1, 'Unit price': parseFloat(e.Price).toFixed(2), 'Subtotal': parseFloat(e.Price).toFixed(2)})
         } else {
             arr.forEach(ov => {
                 if (ov.Name===e.Name) {
                     parseInt(ov.Quantity = ov.Quantity + 1);
-                    parseFloat(ov.Subtotal = ov['Quantity'] * ov['Unit price']);
+                    parseFloat(ov.Subtotal = parseFloat(ov['Quantity'] * ov['Unit price']).toFixed(2));
                 }
             })
         }
@@ -24,11 +20,31 @@ function aggregateSubtotal(input) {
     return arr;
 }
 
-function renderText(params) {
+function isInObject(object,val){
+    return object.some(ov => ov.Name === val)
+}
+
+function renderText(obj) {
     out='***<store earning no money>Receipt ***\n' +
-    //object to string
+    objectToString(obj)+
     '----------------------\n' +
-    `Total: ${} (yuan)\n` +
+    `Total: ${getGrandTotal(obj)} (yuan)\n` +
     '**********************\n';
     return out;
+}
+
+function objectToString(arr) {
+    let s = '';
+    arr.map(o=>{
+        let unit=' bottles'
+        if (o.Name === 'Battery') unit = '';
+        s += `Name: ${o.Name}, Quantity: ${o.Quantity}${unit}, Unit price: ${o['Unit price']} (yuan), Subtotal: ${o['Subtotal']} (yuan)\n`        
+    });
+    return s;
+}
+
+function getGrandTotal(arr) {
+    let total=0;
+    arr.forEach(e=> total = parseFloat(e['Subtotal']) + total)
+    return parseFloat(total).toFixed(2);
 }
